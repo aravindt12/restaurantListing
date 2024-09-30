af857532b6a7ccaab14fb0ed0a5b31c70b3635ec
Aravind Thottempudi <c-aravind.thottempudi1@charter.com>
2024-09-30 13:20:32 -0500

    Initial commit

---
+++ src/main/java/com/aravind/restaurantListing/mapper/RestaurantMapper.java
@@ -0,0 +1,16 @@
+package com.aravind.restaurantListing.mapper;
+
+import com.aravind.restaurantListing.dto.RestaurantDTO;
+import com.aravind.restaurantListing.entity.Restaurant;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface RestaurantMapper {
+
+    RestaurantMapper INSTANCE = Mappers.getMapper(RestaurantMapper.class);
+
+    Restaurant mapRestaurantDTOToRestaurant(RestaurantDTO restaurantDTO);
+
+    RestaurantDTO mapRestaurantToRestaurantDTO(Restaurant restaurant);
+}
